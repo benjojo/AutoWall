7a221a60820bf409dbfa90482e038fcd6508973f
Ben Cox <ben@benjojo.co.uk>
2013-09-01 14:46:15 +0100

    Added the source files.

---
+++ src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace AutoWall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DayOfYear = 0;
+            while (true)
+            {
+                if (DayOfYear != DateTime.Now.DayOfYear - 1)
+                {
+                    DayOfYear = DateTime.Now.DayOfYear -1;
+                    try
+                    {
+                        new WebClient().DownloadFile("http://map2.vis.earthdata.nasa.gov/imagegen/index.php?TIME="+DateTime.Now.Year+""+DayOfYear+"&extent=-5.8232421875,49.599609375,2.6142578125,54.345703125&epsg=4326&layers=MODIS_Terra_CorrectedReflectance_TrueColor,sedac_bound&format=image/jpeg&width=1920&height=1080", "./Today_" + DayOfYear + ".jpg");
+                        Console.WriteLine("Got the file!");
+                        Process process = new Process();
+                        // Configure the process using the StartInfo properties.
+                        process.StartInfo.FileName = Directory.GetCurrentDirectory() + "/WallpaperChanger.exe";
+                        Console.WriteLine("Calling: {0}", Directory.GetCurrentDirectory() + "/WallpaperChanger.exe");
+                        process.StartInfo.Arguments = Directory.GetCurrentDirectory() + "/Today_" + DayOfYear + ".jpg";
+                        process.Start();
+                        process.WaitForExit();// Waits here for the process to exit.
+                    }
+                    catch
+                    {
+                        // Well I tried.
+                    }
+                }
+                Thread.Sleep(60 * 1000);
+            }
+        }
+    }
+}
